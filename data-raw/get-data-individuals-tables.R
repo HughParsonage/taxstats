49dcd86624f47b8bd9967ffd6360f34afac1f0fe
HughParsonage <hugh.parsonage@gmail.com>
2016-04-10 22:04:10 +1000

    Placed in wrong directory.

--- data-raw/get-data-individuals-tables.R
+++ data-raw/get-data-individuals-tables.R
@@ -5,7 +5,9 @@ library(magrittr)
 library(zoo)
 
 # Individuals Table 12
+if (!file.exists("inst/extdata//Individuals-Table-12.xlsx")){
 GET("http://data.gov.au/dataset/25e81c18-2083-4abe-81b6-0f530053c63f/resource/4878bd4b-a03f-4133-9cdc-a4a2bf9be375/download/Taxstats2014Individual12ResidencyTaxableStatusTaxableIncome.xlsx", write_disk("inst/extdata//Individuals-Table-12.xlsx"))
+}
 
 indiv_by_taxable_status_residency_taxable_income_range <- 
   read_excel("inst/extdata/Individuals-Table-12.xlsx", sheet = 2, na = "na", skip = 2)
