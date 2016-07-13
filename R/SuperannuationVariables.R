79a4c5637be191188e65d3a3ef387bdd31042045
HughParsonage <hugh.parsonage@gmail.com>
2016-07-13 12:52:28 +1000

    Initial commit of SuperannuationVariables
    Define pre_tax_contributions.

---
+++ R/SuperannuationVariables.R
@@ -0,0 +1,22 @@
+#' @title Superannuation variables
+#' 
+#' @description A way to add superannuation variables to sample files. 
+#' 
+#' @param .sample.file A sample file onto which new columns are to be added.
+#' @return A data.table 
+#' @import data.table
+#' @export
+
+mutate_super_vars <- function(.sample.file){
+  stopifnot(data.table::is.data.table(.sample.file))
+  
+  if (!all(c("MCS_Emplr_Contr", 
+             "MCS_Prsnl_Contr", 
+             "MCS_Othr_Contr", 
+             "MCS_Ttl_Acnt_Bal") %in% names(.sample.file))){
+    stop("The sample file you requested does not have the variables needed for this function.")
+  }
+  
+  # Pre-tax contributions
+  .sample.file[ , "pre_tax_contributions" := rowSums(.SD), .SDcols = c("MCS_Emplr_Contr", "Rptbl_Empr_spr_cont_amt")]
+}
