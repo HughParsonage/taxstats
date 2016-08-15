7c754c895d1db46696dda9d109e794a62b512570
HughParsonage <hugh.parsonage@gmail.com>
2016-08-15 14:56:09 +1000

    Manual for sample file 1314
    Funds data, documentation

--- data-raw/funds-table-1-2013-14.R
+++ data-raw/funds-table-1-2013-14.R
@@ -1,29 +1,31 @@
+library(httr)
 library(readxl)
 library(dplyr)
-library(tidyr)
-library(httr)
 library(data.table)
+library(dtplyr)
+library(tidyr)
+library(magrittr)
+library(tidyr)
 library(zoo)
 library(grattan)
 
-if (!file.exists("./data-raw/Individuals_table1_2013-14.xlsx")){
-  GET(url = "http://data.gov.au/dataset/25e81c18-2083-4abe-81b6-0f530053c63f/resource/3cd6dee1-785f-4876-a282-ebcf00f9949a/download/Taxstats2014Individual01SelectedItemsByYear.xlsx", 
-      write_disk("./data-raw/Individuals_table1_2013-14.xlsx"))
-}
+temp <- tempfile(fileext = ".xlsx")
+GET(url = "http://data.gov.au/dataset/25e81c18-2083-4abe-81b6-0f530053c63f/resource/15981dd2-ed4a-44e4-8def-0ccfc0ef8090/download/taxstats2014fund1apraselecteditemsbyyear.xlsx", 
+    write_disk(path = temp))
 
-individuals_table1_201314 <- 
+funds_table1_201314 <- 
   
-  read_excel("data-raw/Individuals_table1_2013-14.xlsx", sheet = 2, skip = 2, na = "na") %>% 
-  # 2 is the unit: `no.` or `$`
+  read_excel(temp, sheet = 2, skip = 2, na = "na") %>%
   as.data.table %>%
+  setnames(1, "Selected items") %>%
   setnames(2, "unit") %>%
   # .absent signifies where unit is blank which appears to correspond to
   # selected items that are no longer 'on the form'. See xlsx file, sheet 1.
   # We use n() to ensure that every column at spread() is unique
   # Superheadings are in `Selected items but have a blank before them
   mutate(Superheading = ifelse(is.na(lag(`Selected items`, default = "")) & is.na(unit), 
-                             `Selected items`, 
-                             NA_character_), 
+                               `Selected items`, 
+                               NA_character_), 
          Superheading = na.locf(Superheading, na.rm = FALSE), 
          Superheading = ifelse(is.na(Superheading), "National counts", Superheading)) %>% 
   # ncol(.) - 2L because Superheading is guaranteed to be non-NA (Selected items)
@@ -41,4 +43,4 @@ individuals_table1_201314 <-
   select(-contains("absent")) %>%
   select(Superheading, Selected_items = `Selected items`, fy_year, Count, Sum)
 
-devtools::use_data(individuals_table1_201314)
+devtools::use_data(funds_table1_201314)
--- data-raw/funds-table-2-2013-14.R
+++ data-raw/funds-table-2-2013-14.R
@@ -1,39 +1,47 @@
+library(httr)
 library(readxl)
 library(dplyr)
-library(tidyr)
-library(httr)
 library(data.table)
+library(dtplyr)
+library(tidyr)
+library(magrittr)
+library(tidyr)
 library(zoo)
 library(grattan)
 
-if (!file.exists("./data-raw/Individuals_table1_2013-14.xlsx")){
-  GET(url = "http://data.gov.au/dataset/25e81c18-2083-4abe-81b6-0f530053c63f/resource/3cd6dee1-785f-4876-a282-ebcf00f9949a/download/Taxstats2014Individual01SelectedItemsByYear.xlsx", 
-      write_disk("./data-raw/Individuals_table1_2013-14.xlsx"))
-}
+temp <- tempfile(fileext = ".xlsx")
+GET(url = "http://data.gov.au/dataset/25e81c18-2083-4abe-81b6-0f530053c63f/resource/68ac57dd-1660-4aa0-be5a-f0b9a51064a2/download/taxstats2014fund2smsfselecteditemsbyyear.xlsx", 
+    write_disk(path = temp))
 
-individuals_table1_201314 <- 
+funds_table2_smsf_201314 <- 
   
-  read_excel("data-raw/Individuals_table1_2013-14.xlsx", sheet = 2, skip = 2, na = "na") %>% 
-  # 2 is the unit: `no.` or `$`
+  read_excel(temp, sheet = 2, skip = 2, na = "na") %>%
   as.data.table %>%
+  setnames(1, "Selected items") %>%
   setnames(2, "unit") %>%
+  {
+    dot <- .
+    kill <- which(sapply(dot, function(v) all(is.na(v))))
+    select(dot, -kill)
+  } %>%
   # .absent signifies where unit is blank which appears to correspond to
   # selected items that are no longer 'on the form'. See xlsx file, sheet 1.
   # We use n() to ensure that every column at spread() is unique
   # Superheadings are in `Selected items but have a blank before them
   mutate(Superheading = ifelse(is.na(lag(`Selected items`, default = "")) & is.na(unit), 
-                             `Selected items`, 
-                             NA_character_), 
+                               `Selected items`, 
+                               NA_character_), 
          Superheading = na.locf(Superheading, na.rm = FALSE), 
          Superheading = ifelse(is.na(Superheading), "National counts", Superheading)) %>% 
   # ncol(.) - 2L because Superheading is guaranteed to be non-NA (Selected items)
   filter(rowSums(is.na(.)) < ncol(.) - 2L) %>%
   group_by(`Selected items`) %>%
+  mutate(unit = tolower(unit)) %>%
   mutate(unit = ifelse(is.na(unit), paste0(".absent", 1:n()),
                        ifelse(unit == "no.", "Count", 
                               ifelse(unit == "$", "Sum", "unknown")))) %>% 
   gather(fy_year, value, -`Selected items`, -Superheading, -unit, factor_key = TRUE) %>%
-  data.table %>% 
+  as.data.table(.) %>% 
   spread(unit, value) %>% 
   # fy_year are inconsisently coded. N.B. 1999-00 is coded as 1999-2000
   mutate(fy_year = yr2fy(as.numeric(gsub("^([12][0-9]{3})[^0-9](([0-9]{2})|(2000))$", "\\1", fy_year)) + 1)) %>%
@@ -41,4 +49,4 @@ individuals_table1_201314 <-
   select(-contains("absent")) %>%
   select(Superheading, Selected_items = `Selected items`, fy_year, Count, Sum)
 
-devtools::use_data(individuals_table1_201314)
+devtools::use_data(funds_table2_smsf_201314)
