4277221bff51495ddd9c3a29293ae83793d578fc
HughParsonage <hugh.parsonage@gmail.com>
2016-12-03 18:08:28 +1100

    CRAN note avoidance

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,9 @@
 
-.onAttach <-function(libname = find.package("taxstats"), pkgname = "taxstats"){
-  NULL
+.onLoad <-function(libname = find.package("taxstats"), pkgname = "taxstats"){
+  if (getRversion() >= "2.15.1"){
+    utils::globalVariables(c("sample_file_0304", "sample_file_0405", "sample_file_0506", "sample_file_0607",
+                             "sample_file_0708", "sample_file_0809", "sample_file_0910", "sample_file_1011",
+                             "sample_file_1112", "sample_file_1213", "sample_file_1314"))
+  }
 }
 
